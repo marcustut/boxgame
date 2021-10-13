518f9a1d940ec6c9176d7a80d34a05b5322e5f54
Andrehjz <andrehim@dappnology.com>
2021-10-13 23:44:22 +0800

    BSG v1

---
+++ web/src/features/login/routes/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/style.css'
+
+export const Login = () => {
+  return (
+    <div className='bg-light-900'>
+      <h1>WE are the BSG de Pros</h1>
+    </div>
+  )
+}
