a1d3911c7d59eb5a00e421e49e8a03bf5c8b719c
Andrehjz <andrehim@dappnology.com>
2021-10-18 15:02:37 +0800

    Andre initial commit: Login

--- web/src/features/login/routes/Login.tsx
+++ web/src/features/login/routes/Login.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { FaUser } from 'react-icons/fa'
+import { FaUnlock } from 'react-icons/fa'
 import '../styles/style.css'
+
+const submitHandler = (e: React.FormEvent<HTMLElement>) => {
+  e.preventDefault()
+}
 
 export const Login = () => {
   return (
-    <div className='bg-light-900'>
-      <h1>WE are the BSG de Pros</h1>
-    </div>
+    <form onSubmit={submitHandler}>
+      <div className='form-in'>
+        <h1>Log In</h1>
+        <div className='box'>
+          <div className='user'>
+            <FaUser />
+            <input type='text' name='username' id='username' autoComplete='off' placeholder='Username/Email' />
+            <br />
+            <FaUnlock />
+            <input type='password' name='password' id='password' autoComplete='off' placeholder='Password' />
+          </div>
+          {/* Error */}
+          <p className='reset-password'>Forgot Password?</p>
+          <div className='login-btn'>
+            <button className='btn'>Login</button>
+          </div>
+        </div>
+      </div>
+    </form>
   )
 }
